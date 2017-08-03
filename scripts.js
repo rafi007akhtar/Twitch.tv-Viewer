d5b7f87c529e80ab36691b0561f178644bcb1ddb
Md Rafi Akhtar <alimdrafi@gmail.com>
2017-08-04 00:58:23 +0530

    1. (Sort of) Finished search bar 2. Added a footer, thus completing the project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+$(".foot").hide();
 var tbody = $("#twitch-body");
 var thead = $("#twitch-head");
 
@@ -12,15 +13,22 @@ for (i = 0; i < 8; i++) {
         dataType: "jsonp",
         success: function(data) {
             var content = $("<li class='offs'></li>");
+            if(data == null) {
+                content.append("404 not found");
+                return;
+            }
             if (data.logo == null)
-                content.append("<img src='https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png' alt='User logo' class='col-xs-4'>")
+                content.append("<img src='https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png' alt='User logo' class='col-xs-4'>");
             else
-                content.append("<img src='" + data.logo + "' alt='User logo' class='col-xs-4'>")
-            content.append("<span class='col-xs-8 user-details'> <a href='https://www.twitch.tv/" + data.name + "' target='_blank'>" + data.display_name + " </a> <br> <span id='" + data.name + "' class='text-right'></span> </span>")
+                content.append("<img src='" + data.logo + "' alt='User logo' class='col-xs-4'>");
+            content.append("<span class='col-xs-8 user-details'> <a href='https://www.twitch.tv/" + data.name + "' target='_blank'>" + data.display_name + " </a> <br> <span id='" + data.name + "' class='text-right'></span> </span>");
             content.addClass("twitch-user twitch-border-all row");
+            content.addClass(data.display_name);
             tbody.append(content);
         }
+        
     });
+    $(".foot").show();
 }
 
 var myurl = "https://wind-bow.gomix.me/twitch-api/streams/";
@@ -118,6 +126,21 @@ off.click(function() {
 });
 
 var inp = $("input");
-inp.keypress(function (key) {
-    console.log(inp.val());
+inp.keypress(function (key) {    
+    var names = ["ESL_SC2", "OgamingSC2", "storbeck", "cretetion", "FreeCodeCamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    var text = inp.val().toLowerCase();
+    for(i = 0; i < 8; i++) {
+        var temp = $("." + names[i]);
+        if((temp[0].innerText.toLowerCase()).search(text) === -1) {
+            //console.log(text);
+            temp.hide();
+        }
+    }
+});
+
+inp.keyup(function(key) {
+    if(key.keyCode == 8 || key.keyCode == 46) {
+        $("input").val("");
+        all.click();
+    }
 });
